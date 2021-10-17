d67b4a976202535771961a42ac2652dbd3e7754b
student <student@COMP-507-1-03>
2021-10-17 09:36:23 +0300

    Fix

--- ProductView_v_4/ProductView_v_4/Controllers/HomeController.cs
+++ ProductView_v_4/ProductView_v_4/Controllers/HomeController.cs
@@ -18,14 +18,54 @@ namespace ProductView_v_4.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() => View(new IndexModel());
+
+        public IActionResult NewProduct() => View();
+
+        public IActionResult Del(int id)
         {
-            return View();
+            ProductRepository.Delete(id);
+            return Redirect("/Home/Index");
+        }
+        public IActionResult EditView(int id) => View(ProductRepository.GetProducts().Find(i => i.id == id));
+
+        public IActionResult EditProduct(Product product)
+        {
+            if (CheckProduct(product))
+                ProductRepository.UpdateProducts(product);
+            return Redirect("/Home/Index");
         }
 
-        public IActionResult Privacy()
+        public IActionResult Add(Product product)
         {
-            return View();
+            if (CheckProduct(product))
+                ProductRepository.AddProducts(product);
+
+            return Redirect("/Home/NewProduct");
+        }
+
+        private bool CheckProduct(Product product)
+        {
+            bool rez = false;
+            if (product.model != null && product.model != string.Empty)
+                if (product.product != null && product.product != string.Empty)
+                    if (product.salesman != null && product.salesman != string.Empty)
+                        if (product.category != null && product.category != string.Empty)
+                            if (product.description != null && product.description != string.Empty)
+                                if (product.price != null)
+                                    if (product.img != null && product.img != string.Empty && IsImg(product.img))
+                                        rez = true;
+            return rez;
+        }
+
+        private bool IsImg(string path)
+        {
+            bool rez;
+            if (path.EndsWith(".png") || path.EndsWith(".jpg"))
+                rez = true;
+            else
+                rez = false;
+            return rez;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
